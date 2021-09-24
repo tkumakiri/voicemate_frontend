68d5a3a91a360bf2c4cb7f44d7d56fe73242836e
takahiro20 <itnglaac@gmail.com>
2021-09-24 15:46:54 +0900

    SelectRoomに合わせてAppとindexの書き換え

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -2,3 +2,4 @@ export { default as SignUp } from './SignUp';
 export { default as SignIn } from './SignIn';
 export { default as Top } from './Top';
 export { default as Home } from './Home';
+export { default as SelectRoom } from './SelectRoom';
