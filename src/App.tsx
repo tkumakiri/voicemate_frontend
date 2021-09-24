19d7b90c59bfbfb7a5c89d621cf9478b04e09c59
Rai <raimi.rarara@gmail.com>
2021-09-24 16:04:31 +0900

    feat: profileページ

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { CreateRoom, Home, SignIn, SignUp, Top } from './templates';
+import { CreateRoom, Home, SignIn, SignUp, Top, Profile } from './templates';
 
 function App() {
     return (
@@ -13,6 +13,7 @@ function App() {
                 <Route exact path={'/signin'} component={SignIn} />
                 <Route exact path={'/home'} component={Home} />
                 <Route exact path={'/createroom'} component={CreateRoom} />
+                <Route exact path={'/profile'} component={Profile} />
             </Switch>
         </BrowserRouter>
     );
--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -3,3 +3,4 @@ export { default as SignIn } from './SignIn';
 export { default as Top } from './Top';
 export { default as Home } from './Home';
 export { default as CreateRoom } from './CreateRoom';
+export { default as Profile } from './Profile';
