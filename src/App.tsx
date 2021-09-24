b5fec18cca8d6712b69f0762b8b2761ab672c4b7
Rai <raimi.rarara@gmail.com>
2021-09-24 16:14:41 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { CreateRoom, Home, SignIn, SignUp, Room,  Top, Profile, SelectRoom, TestSkyway } from './templates';
+import { CreateRoom, Home, SignIn, SignUp, Room, Top, Profile, SelectRoom, TestSkyway } from './templates';
 
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path={'/'} component={Top} />
-                <Route exact path={'/signup'} component={SignUp} />
-                <Route exact path={'/signin'} component={SignIn} />
-                <Route exact path={'/home'} component={Home} />
-
-                <Route exact path={'/testskyway'} component={TestSkyway} />
-
-
-                <Route exact path={'/select'} component={SelectRoom} />
-
-                <Route exact path={'/room'} component={Room} />
-
-                <Route exact path={'/createroom'} component={CreateRoom} />
-                <Route exact path={'/profile'} component={Profile} />
-
-
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={'/'} component={Top} />
+        <Route exact path={'/signup'} component={SignUp} />
+        <Route exact path={'/signin'} component={SignIn} />
+        <Route exact path={'/home'} component={Home} />
+        <Route exact path={'/testskyway'} component={TestSkyway} />
+        <Route exact path={'/selectroom'} component={SelectRoom} />
+        <Route exact path={'/room'} component={Room} />
+        <Route exact path={'/createroom'} component={CreateRoom} />
+        <Route exact path={'/profile'} component={Profile} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
