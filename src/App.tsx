1599d5fe80eaf5bef9420945198f32c73c9d0b0a
Rai <raimi.rarara@gmail.com>
2021-09-27 01:04:50 +0900

    feat: searchroomページ追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { CreateRoom, Home, SignIn, SignUp, Room, Top, Profile, SelectRoom, TestSkyway } from './templates';
+import { CreateRoom, Home, SignIn, SignUp, Room, Top, Profile, SelectRoom, TestSkyway, SearchRoom } from './templates';
 
 
 function App() {
@@ -19,6 +19,7 @@ function App() {
         <Route exact path={'/room'} component={Room} />
         <Route exact path={'/createroom'} component={CreateRoom} />
         <Route exact path={'/profile'} component={Profile} />
+        <Route exact path={'/searchroom'} component={SearchRoom} />
       </Switch>
     </BrowserRouter>
   );
